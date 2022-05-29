c218cbafbbfe9ca79d4059c9ba9adfad23b042fd
fomka94 <fom-aina@mail.ru>
2022-05-29 20:37:15 +0900

    Initial Commit

---
+++ xxxxx11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xxxxx11
+//
+//  Created by Фомина Айина Валерьевна on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
